96bf7aae41c7c7a0ebc22fa5e2f564659e95eb87
Saichul Huda <saichulhuda1995@gmail.com>
2026-01-03 00:47:21 +0700

    fix qeury lambat

--- ajax/lap-rekapitulasi/cetak-lap-rekapitulasi.php
+++ ajax/lap-rekapitulasi/cetak-lap-rekapitulasi.php
@@ -25,14 +25,12 @@ $acc_condition = ($kd_rekening != '' ? "(a.kd_rekening='" . $kd_rekening . "') A
 $acc_condition2 = ($kecamatan != '' ? "(wp_wr_camat='" . $kecamatan . "') AND" : "");
 
 if ($tipe_retribusi == '1') {
-	$list_sql = "SELECT a.bln_retribusi,a.thn_retribusi,a.total_retribusi,b.*, c.rrn FROM app_nota_perhitungan as a 
-					 INNER JOIN 
-					 	(SELECT x.id_skrd,x.no_skrd,to_char(x.tgl_skrd,'DD-MM-YYYY') as tgl_skrd,x.wp_wr_nama,x.wp_wr_alamat,x.wp_wr_camat,x.kd_billing as kode_billing ,y.* FROM app_skrd as x 
-					 		LEFT JOIN (SELECT ntpd,to_char(tgl_pembayaran,'DD-MM-YYYY') as tgl_pembayaran,denda,total_bayar,kd_billing FROM app_pembayaran_retribusi) as y ON (x.kd_billing=y.kd_billing)
-					 	 " . $skrd_cond . ") as b 
-					 ON (a.fk_skrd=b.id_skrd)
-					 LEFT JOIN log_payment_qris as c ON (b.kd_billing=c.kode_billing)
-					 WHERE " . $acc_condition . " " . $acc_condition2 . "(a.bln_retribusi='" . $bln_retribusi . "') AND (a.thn_retribusi='" . $thn_retribusi . "')";
+	$list_sql = "SELECT a.bln_retribusi,a.thn_retribusi,a.total_retribusi,b.*, c.rrn 
+					FROM app_nota_perhitungan as a 
+					INNER JOIN (SELECT x.id_skrd,x.no_skrd,to_char(x.tgl_skrd,'DD-MM-YYYY') as tgl_skrd,x.wp_wr_nama,x.wp_wr_alamat,x.wp_wr_camat,x.kd_billing as kode_billing, x.status_ketetapan,y.* FROM app_skrd as x LEFT JOIN (SELECT ntpd,to_char(tgl_pembayaran,'DD-MM-YYYY') as tgl_pembayaran,denda,total_bayar,kd_billing FROM app_pembayaran_retribusi) as y ON (x.kd_billing::text=y.kd_billing::text) ) as b ON (a.fk_skrd=b.id_skrd) 
+					LEFT JOIN log_payment_qris as c ON (b.kd_billing::text=c.kode_billing::text)
+					 WHERE " . $acc_condition . " " . $acc_condition2 . "(a.bln_retribusi='" . $bln_retribusi . "') AND (a.thn_retribusi='" . $thn_retribusi . "')
+					 AND (b.status_ketetapan='1')";
 } else {
 	$list_sql = "SELECT 
 					 (SELECT to_char(tgl_pengembalian,'DD-MM-YYYY') as tgl_pengembalian FROM app_pengembalian_karcis as x WHERE(x.fk_permohonan=a.id_permohonan) ORDER BY id_pengembalian ASC LIMIT 1)  as tgl_pengembalian,
